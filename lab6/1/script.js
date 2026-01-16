32ece7bc4b131c24fe0c002f8e31b2e44d26df57
Phoz07 <maverxk07@gmail.com>
2026-01-16 14:30:35 +0700

    add some content

---
+++ lab6/1/script.js
@@ -0,0 +1,32 @@
+const tableBody = document.getElementById("table-body");
+
+const handleFetch = () => {
+  const data = fetch("./employees.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      renderTable(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+  return data;
+};
+
+const renderTable = (data) => {
+  data.map((employee) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${employee.id}</td>
+      <td>${employee.Gender}</td>
+      <td>${employee.FirstName} ${employee.LastName}</td>
+      <td>${employee.Position}</td>
+      <td>${employee.Address}</td>
+    `;
+    tableBody.appendChild(row);
+  });
+};
+
+window.onload = () => {
+  renderTable(handleFetch());
+};
