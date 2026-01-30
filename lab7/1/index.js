3ae2d27d5632e1b15929470ffda76b1417a37580
Phoz07 <maverxk07@gmail.com>
2026-01-30 14:59:12 +0700

    Add lab7.1

---
+++ lab7/1/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.static("images"));
+
+app
+  .get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/home.html"));
+  })
+  .get("/dogs", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/dogs.html"));
+  })
+  .get("/cats", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/cats.html"));
+  })
+  .get("/birds", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/birds.html"));
+  });
+
+app.listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
